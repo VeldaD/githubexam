85c4387b7c6d2185162a4e6db829f41afc3e3035
Velda <velda07dias@gmail.com>
2024-02-01 10:16:06 +0530

    commit

---
+++ CaseToolExam/src/exampackage/Caterpillar_237.java
@@ -0,0 +1,14 @@
+package exampackage;
+
+public class Caterpillar_237 {
+
+	int moves,cabbages;
+
+	Caterpillar_237(int moves,int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages ;
+
+	}
+
+}
